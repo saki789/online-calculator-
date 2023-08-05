4e6a30cafa64479c2c0b04ecf507becec900e615
saki komai <55061969+saki789@users.noreply.github.com>
2023-08-05 23:15:10 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
+function updateDisplay(value) {
+  document.querySelector(".display").value += value;
+}
 
-  function updateDisplay(value) {
-    document.querySelector(".display").value += value;
+function clearDisplay() {
+  document.querySelector(".display").value = "";
+}
+
+function calculateResult() {
+  const input = document.querySelector(".display").value;
+  try {
+    const result = eval(input);
+    document.querySelector(".display").value = result;
+  } catch (error) {
+    document.querySelector(".display").value = "Error";
   }
+}
 
-  function clearDisplay() {
-    document.querySelector(".display").value = "";
-  }
-
-  function calculateResult() {
-    const input = document.querySelector(".display").value;
-    try {
-      const result = eval(input);
-      document.querySelector(".display").value = result;
-    } catch (error) {
-      document.querySelector(".display").value = "Error";
-    }
-  }
-
-  document.addEventListener("DOMContentLoaded", () => {
-    const buttons = document.querySelectorAll(".buttons button");
-    buttons.forEach((button) => {
-      button.addEventListener("click", () => {
-        const buttonText = button.textContent;
-        if (buttonText === "C") {
-          clearDisplay();
-        } else if (buttonText === "=") {
-          calculateResult();
-        } else {
-          updateDisplay(buttonText);
-        }
-      });
+document.addEventListener("DOMContentLoaded", () => {
+  const buttons = document.querySelectorAll(".buttons button");
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const buttonText = button.textContent;
+      if (buttonText === "C") {
+        clearDisplay();
+      } else if (buttonText === "=") {
+        calculateResult();
+      } else {
+        updateDisplay(buttonText);
+      }
     });
   });
+});
